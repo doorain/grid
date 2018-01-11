9a73555b4f8179ac3c5c72fcc0e8b3e664bc5300
Dorian Morgan <doorain@gmail.com>
2018-01-11 13:21:22 -0700

    row height and grid gap

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
         <div id="grid-content">
           <div>1</div>
           <div>2</div>
-          <div>3</div>
+          <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus nulla at volutpat diam ut. Pharetra massa massa ultricies mi. Ut etiam sit amet nisl purus in. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Quis eleifend quam adipiscing vitae proin. Feugiat scelerisque varius morbi enim. Arcu bibendum at varius vel pharetra vel turpis. Fames ac turpis egestas maecenas pharetra convallis. Elementum tempus egestas sed sed. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce.</div>
           <div>4</div>
           <div>5</div>
           <div>6</div>
