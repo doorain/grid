2cc9f5b16447c4fd8ccf6052978325d6d86da847
Dorian Morgan <doorain@gmail.com>
2018-01-11 13:37:02 -0700

    column and row lines

--- src/App.js
+++ src/App.js
@@ -6,15 +6,13 @@ class App extends Component {
     return (
       <div className="App">
         <div id="grid-content">
-          <div>1</div>
-          <div>2</div>
-          <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus nulla at volutpat diam ut. Pharetra massa massa ultricies mi. Ut etiam sit amet nisl purus in. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Quis eleifend quam adipiscing vitae proin. Feugiat scelerisque varius morbi enim. Arcu bibendum at varius vel pharetra vel turpis. Fames ac turpis egestas maecenas pharetra convallis. Elementum tempus egestas sed sed. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce.</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-          <div>7</div>
-          <div>8</div>
-          <div>9</div>
+          <div class="one">1</div>
+          <div class="two">2</div>
+          <div class="three">3</div>
+          <div class="four">4</div>
+          <div class="five">5</div>
+          <div class="six">6</div>
+
         </div>
       </div>
     );
